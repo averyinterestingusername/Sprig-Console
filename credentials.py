bd9e8455f2d0bc9c3c56a93358b33696704ec463
averyinterestingusername <electriconicmail@gmail.com>
2025-01-18 20:10:58 +0100

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+SERVER_HOST = ''  # Your PC's IP address
+SERVER_PORT = 34567  # A port, make sure it's not reserved
+SSID = ''  # The name of your WiFi network
+PASSWORD = ''  # The password of you WiFi network
